c1ae30f10d0305969a4c438260946b2064e32d88
Yolantika Nur <yolantikan34@gmail.com>
2022-04-02 21:21:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+     <meta name="description" content="YBRevolution">
+      <meta name="keywords" content="Penyedia jasa mentoring tugas akhir dan pembuatan website di Sanggau, Pontianak, Kalimantan Barat">
+      <meta name="author" content="YBRevolution">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <script src="https://kit.fontawesome.com/5704997521.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/custom.css" type="text/css">
+
+    <title>YBRevolution</title>
+  </head>
+  <body>
+   <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+   	<div class="container">
+  <a class="navbar-brand" href="#">YBRevolution</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mentoring</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Umum</a>
+      </li>
+    </ul>
+</div>
+  </div>
+</nav>
+
+			<!-- banner -->
+			<div class="container-fluid my-banner">
+			<h1><span class="txtybr">Halo</span>, Selamat Datang!</h1><br>
+			<h3>di <!-- <span class="txtybr">Price List</span>  --><span class="txtybr">YBRevolution</span></h3><br>
+			<h5>#mentorinAja | #dibawaMudahAja</h5>
+			<br>
+			</div>
+			<!-- mentoring -->
+			<div class="container-fluid why-mentoring">
+				<h1 class="title-section">Mentoring</h1>
+				<div class="row">
+					<div class="col-sm-6">
+						<p>YBRevolution menyediakan wadah bagi mahasiswa / mahasiswa akhir IT untuk mendapatkan kemudahan dalam menyusun serta memahami laporan tugas akhir / skripsi dan dipandu oleh mentor yang berpengalaman.</p>
+						<button type="button" class="btn btn-primary btn-ybr">Lihat Paket Mentoring</button>
+					</div>
+					<div class="col-sm-6">
+						<img src="assets/img/4905784.jpg">
+					</div>
+				</div>
+			</div>
+
+			<!-- umum -->
+			<div class="container-fluid umum">
+				<h1 class="title-section">Umum</h1>
+				<div class="row">
+					<div class="col-sm-6">
+						<img src="assets/img/umum.png">
+					</div>
+					<div class="col-sm-6">
+						<p>YBRevolution memperhatikan bahwa website merupakan salah satu sarana penting untuk .</p>
+						<button type="button" class="btn btn-primary btn-ybr">Lihat Paket Mentoring</button>
+					</div>
+				</div>
+			</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
